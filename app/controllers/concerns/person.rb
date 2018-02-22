df1bd929ab4116cac28649d1c90d231cacfa735e
Emily Zhong <emilyzhongsihua@gmail.com>
2018-02-22 13:50:33 -0800

    completed

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,21 +4,23 @@ class Person
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname = name[0,4]
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    # nickname
+    @nickname
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    Time.now.year - @age.to_i
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    "#{@name} is #{@age} years old!"
   end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,8 @@ class PagesController < ApplicationController
   def person
     @person = Person.new params[:name], params[:age]
   end
+
+  def me
+    @stuff = "I'm Emily! I'm a sophomore from San Jose. One fun fact is that one of my limbs is longer than the others, and I'm excited to learn from gr9 instructors this semester!"
+  end
 end
